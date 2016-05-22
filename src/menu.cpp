5308d516ad4f2dec01cdcd8ae9d42fea799c4675
Ghislain <ghitho@users.noreply.github.com>
2016-05-22 22:02:30 +0200

    add button quit

--- src/menu.cpp
+++ src/menu.cpp
@@ -3,24 +3,31 @@
 
 void showMenu(MainWindow *window, SDLPoller *main_poller)
 {
+	bool request_close = false;
 	Button play_button(window, 0, 40, "ressources/play_button.bmp", "Play");
 	Button param_button(window, 0, 100, "ressources/play_button.bmp", "Parameters");
+	Button quit_button(window, 0, 160, "ressources/play_button.bmp", "Quit");
 
 	play_button.setPosition(window->getWidth() / 2 - play_button.getWidth() / 2, 40);
-	param_button.setPosition(window->getWidth() / 2 - play_button.getWidth() / 2, 100);
+	param_button.setPosition(window->getWidth() / 2 - param_button.getWidth() / 2, 100);
+	quit_button.setPosition(window->getWidth() / 2 - quit_button.getWidth() / 2, 160);
 	main_poller->subscribe(&play_button);
 	main_poller->subscribe(&param_button);
-	while (!window->hasCloseRequest())
+	main_poller->subscribe(&quit_button);
+	while (!window->hasCloseRequest() && !request_close)
 	{
 		main_poller->Poll();
 		window->clear();
 		play_button.draw();
 		param_button.draw();
+		quit_button.draw();
 		window->update();
 		if (play_button.isClicked())
 			showGame(window, main_poller);
 		if (param_button.isClicked())
 			showParameters(window, main_poller);
+		if (quit_button.isClicked())
+			request_close = true;
 		SDL_Delay(20);
 	}
 	main_poller->unSubscribe(&play_button);
