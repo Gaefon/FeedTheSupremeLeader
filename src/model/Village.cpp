9821ec59e616de426b58623e80410570769c4130
Ghislain <ghitho@users.noreply.github.com>
2016-06-16 22:56:45 +0200

    Bugfix : housed population cannot be greater than population anymore

--- src/model/Village.cpp
+++ src/model/Village.cpp
@@ -28,117 +28,119 @@ void Village::setPopulation(unsigned int population)
 {
     m_population = population;
 }
-    unsigned int Village::getHousedPopulation()
-    {
-        return m_housed_population;
-    }
-    void Village::setHousedPopulation(unsigned int population)
-    {
-        m_housed_population = population;
-    }
-    unsigned int Village::getSchooledPopulation()
-    {
-        return m_schooled_population;
-    }
 
-    void Village::setHousingCapacity(unsigned int capacity)
-    {
-        m_housing_capacity = capacity;
-    }
-    unsigned int Village::getHousingCapacity()
-    {
-        return m_housing_capacity;
-    }
+unsigned int Village::getHousedPopulation()
+{
+    return m_housed_population;
+}
+void Village::setHousedPopulation(unsigned int population)
+{
+    m_housed_population = population;
+}
+unsigned int Village::getSchooledPopulation()
+{
+    return m_schooled_population;
+}
 
-    void Village::setSchooledPopulation(unsigned int population)
-    {
-        m_schooled_population = population;
-    }
+void Village::setHousingCapacity(unsigned int capacity)
+{
+    m_housing_capacity = capacity;
+}
+unsigned int Village::getHousingCapacity()
+{
+    return m_housing_capacity;
+}
 
-    unsigned int Village::getWorkers()
-    {
-        return m_population;
-    }
+void Village::setSchooledPopulation(unsigned int population)
+{
+    m_schooled_population = population;
+}
 
-    void Village::setWorkers(unsigned int workers)
-    {
-        m_workers = workers;
-    }
-    unsigned int Village::getFood()
-    {
-        return m_food;
-    }
-    void Village::setFood(unsigned int food)
-    {
-        m_food = food;
-    }
+unsigned int Village::getWorkers()
+{
+    return m_population;
+}
 
-    int Village::getMerit()
-    {
-        return m_merit;
-    }
-    void Village::setMerit(int merit)
-    {
-        m_merit = merit;
-    }
-    int Village::getFavor()
-    {
-        return m_favor;
-    }
-    void Village::setFavor(int favor)
-    {
-        m_favor = favor;
-    }
-    int Village::getWeightContribution()
-    {
-        return m_weight_contribution;
-    }
-    void Village::setWeightContribution(int weight_contribution)
-    {
-        m_weight_contribution = weight_contribution;
-    }
+void Village::setWorkers(unsigned int workers)
+{
+    m_workers = workers;
+}
+unsigned int Village::getFood()
+{
+    return m_food;
+}
+void Village::setFood(unsigned int food)
+{
+    m_food = food;
+}
 
-    Map *Village::getMap()
-    {
-        return m_map;
-    }
-    void Village::setMap(Map *game_map)
-    {
-        m_map = game_map;
-    }
+int Village::getMerit()
+{
+    return m_merit;
+}
+void Village::setMerit(int merit)
+{
+    m_merit = merit;
+}
+int Village::getFavor()
+{
+    return m_favor;
+}
+void Village::setFavor(int favor)
+{
+    m_favor = favor;
+}
+int Village::getWeightContribution()
+{
+    return m_weight_contribution;
+}
+void Village::setWeightContribution(int weight_contribution)
+{
+    m_weight_contribution = weight_contribution;
+}
 
-    void Village::distributePopulation()
+Map *Village::getMap()
+{
+    return m_map;
+}
+void Village::setMap(Map *game_map)
+{
+    m_map = game_map;
+}
+
+void Village::distributePopulation()
+{
+    list<Building *>::iterator it;
+
+    while (m_population > m_housed_population && m_housed_population < m_housing_capacity)
     {
-        list<Building *>::iterator it;
-        while (m_population > m_housed_population && m_housed_population < m_housing_capacity)
+        for (it = getMap()->getBuildings()->begin(); it != getMap()->getBuildings()->end() && m_population > m_housed_population; it++)
         {
-            for (it =  getMap()->getBuildings()->begin(); it !=  getMap()->getBuildings()->end(); it++)
+            if((*it)->getMaxOccupancy() - (*it)->getOccupancy() > 0)
             {
-                if((*it)->getMaxOccupancy() - (*it)->getOccupancy() > 0)
-                {
-                  (*it)->setOccupancy((*it)->getOccupancy() + 1);
-                  m_housed_population ++;
-                }
+                (*it)->setOccupancy((*it)->getOccupancy() + 1);
+                m_housed_population++;
             }
         }
     }
+}
 
-    void Village::managePopulation()
+void Village::managePopulation()
+{
+    float base_growth = m_population * 0.03f;
+
+    if (m_population == 0)
     {
-        float base_growth = m_population * 0.03f;
-
-        if (m_population == 0)
-        {
-            m_population = m_housing_capacity / 2;
-        }
-        else if (m_housing_capacity > m_population)
-        {
-            float base_immigration = (m_housing_capacity - m_population) * HOUSING_ATTRACTIVENESS;
-            float bonus_immigration = base_immigration * (1 + (m_merit / MAX_MERIT * 3 + m_favor / MAX_FAVOR) / 4);
-            float immigrated_population = base_immigration  + bonus_immigration;
-            m_population = m_population + immigrated_population;
-        }
-        m_population = m_population + base_growth;
-        distributePopulation();
+        m_population = m_housing_capacity / 2;
     }
+    else if (m_housing_capacity > m_population)
+    {
+        float base_immigration = (m_housing_capacity - m_population) * HOUSING_ATTRACTIVENESS;
+        float bonus_immigration = base_immigration * (1 + (m_merit / MAX_MERIT * 3 + m_favor / MAX_FAVOR) / 4);
+        float immigrated_population = base_immigration  + bonus_immigration;
+        m_population = m_population + immigrated_population;
+    }
+    m_population = m_population + base_growth;
+    distributePopulation();
+}
 
