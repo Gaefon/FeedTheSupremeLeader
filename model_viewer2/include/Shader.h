21efc6616f71f4746666185a82042d9f003e872a
gaefon <gaetanfon@hotmail.com>
2017-03-27 22:39:27 +0200

    shader class

--- model_viewer2/include/Shader.h
+++ model_viewer2/include/Shader.h
@@ -1,44 +1,25 @@
 #ifndef SHADER_H
 #define SHADER_H
 
-
-#ifdef WIN32
-	#include <GL/glew.h>
-#elif __APPLE__
-	#define GL3_PROTOTYPES 1
-	#include <OpenGL/gl3.h>
-#else
-	#define GL3_1 PROTOTYPES
-	#include <GL/gl.h>
-#endif
-
-
+#include <iostream>
+#include <fstream>
 #include <string>
+#include "Device.h"
 
-class Shader
+namespace GEngine
 {
-    public:
+    class Shader
+    {
+        public:
+            Shader(std::string filename, Device *device);
+            int loadAndReadFile(std::string filename);
+            void createShaderModule(Device *device);
+            VkShaderModule getVulkanObject();
 
-		Shader();
-		Shader(Shader const &copy);
-		Shader(std::string vertex_src, std::string fragment_src);
-		~Shader();
+        private:
+            std::vector<char> buffer;
+            VkShaderModule shader_module;
 
-		Shader& operator=(Shader const &copy);
-
-		bool load();
-		bool compileShader(GLuint &shader, GLenum type, std::string const &source_file);
-		GLuint getProgramID() const;
-
-
-    private:
-
-		GLuint m_vertex_id;
-		GLuint m_fragment_id;
-		GLuint m_program_id;
-
-		std::string m_vertex_source;
-		std::string m_fragment_source;
-};
-
+    };
+}
 #endif
--- model_viewer2/main2.cpp
+++ model_viewer2/main2.cpp
@@ -4,6 +4,7 @@
 #include <Surface.h>
 #include <Device.h>
 #include <SwapChain.h>
+#include <Shader.h>
 
 #include <GLFW/glfw3.h>
 
@@ -18,12 +19,12 @@ int main(void)
 {
 	Window *window;
 	list<string> extensions;
-	
+
 	glfwInit();
-	
+
 	window = new Window("lol", 800, 600);
-	
-	
+
+
 	Engine engine("test", Version::makeVersion(1, 0, 0));
 	engine.addExtension(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
 	engine.pickPhysicalDevices();
@@ -31,11 +32,13 @@ int main(void)
 	PhysicalDevice *phys_dev = surface.getSuitableDevice(&engine);
 	Device dev(phys_dev, engine.getExtensions());
 	SwapChain swap_chain(&surface, window, phys_dev, &dev);
-	
+	Shader shader_frag(string("Shaders/2d_dummy.frag.spv"), &dev);
+	Shader shader_vert(string("Shaders/2d_dummy.vert.spv"), &dev);
+
 	//X11Surface surface(&engine, glfwGetX11Display(), glfwGetX11Window(window));
 	//XcbSurface surface(&engine, nullptr, 42);
 	//WaylandSurface surface(&engine, info.info.wl.display, info.info.wl.surface);
-	
+
 	list<PhysicalDevice *> devs = engine.getListPhysicalDevices();
 	for (list<PhysicalDevice *>::iterator i = devs.begin(); i != devs.end(); i++)
 	{
@@ -45,14 +48,14 @@ int main(void)
 		cout << Version::versionToString((*i)->getApiVersion()) << endl;
 		cout << Version::versionToString((*i)->getDriverVersion()) << endl;
 	}
-	
+
 	while (!window->shouldClose())
 	{
 		glfwPollEvents();
 	}
-	
+
 	delete window;
 	glfwTerminate();
-	
+
 	return 0;
 }
