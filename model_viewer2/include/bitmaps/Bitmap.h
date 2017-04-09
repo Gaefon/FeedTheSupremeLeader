42f47ab32593c177181f0ec9eb5aec96d356fef7
Vermine <ghislain.thomas@laposte.net>
2017-04-09 12:59:38 +0200

    begin bitmap class

---
+++ model_viewer2/include/bitmaps/Bitmap.h
@@ -0,0 +1,17 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+namespace GEngine
+{
+	class Bitmap
+	{
+		private:
+			unsigned int width;
+			unsigned int height;
+		public:
+			Bitmap();
+			~Bitmap();
+	};
+}
+
+#endif
---
+++ model_viewer2/src/bitmaps/Bitmap.cpp
@@ -0,0 +1,14 @@
+#include <bitmaps/Bitmap.h>
+
+namespace GEngine
+{
+	Bitmap::Bitmap()
+	{
+		width = 0;
+		height = 0;
+	}
+	
+	Bitmap::~Bitmap()
+	{
+	}
+}
